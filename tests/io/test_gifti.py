83df47c57a7dba0de0e9e4983a9a73d3655fdff8
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2018-03-17 15:42:15 +0100

    TEST: Add tests for GifTI IO operations
    This commit adds tests for the nimesh.io package. Only saving and
    loading in GifTI is tested.

---
+++ tests/io/test_gifti.py
@@ -0,0 +1,43 @@
+import os
+import tempfile
+import unittest
+
+import numpy as np
+
+import nimesh.io
+from nimesh import Mesh
+
+
+class TestGifTI(unittest.TestCase):
+    """Test the nimesh.io.gifti module."""
+
+    def test_minimal_save_load(self):
+        """Test saving and loading with a minimal mesh."""
+
+        # Create a simple mesh with two triangles.
+        vertices = [
+            [0, 0, 0],
+            [1, 0, 0],
+            [1, 1, 0],
+            [0, 1, 0],
+        ]
+
+        triangles = [
+            [0, 1, 2],
+            [1, 2, 3],
+        ]
+
+        mesh = Mesh(vertices, triangles)
+
+        # Work in a temporary directory. This guarantees cleanup even on error.
+        with tempfile.TemporaryDirectory() as directory:
+
+            # Save the mesh and reload it.
+            filename = os.path.join(directory, 'mesh.gii')
+            nimesh.io.save(filename, mesh)
+            loaded = nimesh.io.load(filename)
+
+            # The loaded data should match the saved data. Because there is no
+            # data manipulation, this should be bit perfect.
+            np.testing.assert_array_equal(mesh.vertices, loaded.vertices)
+            np.testing.assert_array_equal(mesh.triangles, loaded.triangles)
