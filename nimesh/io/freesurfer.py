82f16c06ae0201a6c3585d5a5e02c9f665cf8e99
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2018-03-26 14:52:29 +0200

    FEAT: Add support to save and load segmentation in .annot
    FreeSurfer uses the .annot file format to save segmentation (or
    parcellation) of the cortex. These files currently cannot be
    loaded in nimesh.
    
    This commit adds support to load and save segmentations to
    FreeSurfer .annot files.

--- nimesh/io/freesurfer.py
+++ nimesh/io/freesurfer.py
@@ -1,13 +1,27 @@
+import os
 import warnings
 
 import nibabel.freesurfer as nibfs
 import numpy as np
 
-from nimesh import CoordinateSystem, Mesh
+from nimesh import CoordinateSystem, Mesh, Segmentation
 
 
-def load(surface: str):
-    """Loads a FreeSurfer surface mesh."""
+def load(surface: str, annotation: str = None) -> Mesh:
+    """Loads a FreeSurfer surface mesh.
+
+    Loads a mesh from a FreeSurfer geometry file, optionally loading
+    annotations.
+
+    Args:
+        surface: The FreeSurfer geometry file, e.g. lh.pial or rh.white.
+        annotation (optional): The annotation file to load, for example
+            lh.aparc.DKTatlas.annot or lh.aparc.annot.
+
+    Returns:
+        mesh: The loaded mesh.
+
+    """
 
     # Load the vertices and remove the CRAS translation if it exists.
     vertices, triangles, meta, info = nibfs.read_geometry(surface, True, True)
@@ -17,7 +31,31 @@ def load(surface: str):
     # We supposed the FreeSurfer surface is in RAS.
     mesh = Mesh(vertices, triangles, CoordinateSystem.RAS)
 
+    # Load the annotations, if requested.
+    if annotation is not None:
+        segmentation = load_segmentation(annotation)
+        mesh.add_segmentation(segmentation)
+
     return mesh
+
+
+def load_segmentation(filename: str) -> Segmentation:
+    """Loads a segmentation from a FreeSurfer annot file.
+
+    Loads the a segmentation from a FreeSurfer annotation file. Because annot
+    files do not have a segmentation name, the file name is used instead.
+
+    Args:
+        filename: The name of the annot file that is loaded.
+
+    Returns:
+        segmentation: A segmentation whose name is `filename`.
+
+    """
+
+    labels, ctab, names = nibfs.read_annot(filename, True)
+
+    return Segmentation(os.path.basename(filename), labels)
 
 
 def save(filename: str, mesh: Mesh):
@@ -48,3 +86,20 @@ def save(filename: str, mesh: Mesh):
     }
 
     nibfs.write_geometry(filename, mesh.vertices, mesh.triangles, True, meta)
+
+
+def save_segmentation(filename: str, segmentation: Segmentation):
+    """Saves a segmentation to a FreeSurfer annot file.
+
+    Args:
+        filename: The name of the annotation file where the segmentation will
+            be saved.
+        segmentation: The segmentation to save.
+
+    """
+
+    labels = segmentation.labels
+    ctab = np.array([(i, 0, 0, 0, i) for i in np.unique(labels)],
+                    dtype=np.int16)
+    names = [str(i) for i in np.unique(labels)]
+    nibfs.write_annot(filename, labels, ctab, names)
