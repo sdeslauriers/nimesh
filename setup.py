b34325e5f09a0edf3f601400c76185c2234b83cb
Samuel Deslauriers-Gauthier <sam.deslauriers@gmail.com>
2018-06-14 09:28:37 +0200

    ENH: Add package information to setup
    To distribute the package on PyPi, package information is missing
    from the setup file.
    
    This commit adds the missing information in the setup file.

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='nimesh',
     version='0.0.0',
     packages=['nimesh', 'nimesh.io'],
-    url='https://github.com/sdeslauriers/nimesh.git',
+    url='https://github.com/sdeslauriers/nimesh',
     license='MIT',
     author='Samuel Deslauriers-Gauthier',
     author_email='sam.deslauriers@gmail.com',
     description='A Python package to manipulate neuroimaging triangular '
                 'meshes.',
+    long_description=long_description,
+    long_description_content_type='test/markdown',
     install_requires=['numpy', 'nibabel'],
     extras_requires={
         'visualization': ['vtk'],
-    }
+    },
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.'
+    ),
 )
